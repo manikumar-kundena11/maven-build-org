7c3ea46e3f9ed7d2020703234f31096ca177c595
manikumar-kundena11 <ragavendramanikumar6@gmail.com>
2026-02-11 09:50:33 +0530

    Add main class for Jenkins build success message

---
+++ src/main/java/com/exam/App.java
@@ -0,0 +1,8 @@
+package com.exam;
+public class App {
+  pubic static void main(String[] args){
+    System.out.println("======================================================================");
+    System.out.println("Jenkins Build Syccessfull-----Manikumar Kundena");
+    System.out.println("=======================================================================");
+  }
+}
