a5efb64e0bd6eeb100c87b33ffa781f4c03b7fe6
manikumar-kundena11 <ragavendramanikumar6@gmail.com>
2026-02-11 10:01:21 +0530

    Fix typo in main method declaration

--- src/main/java/com/exam/App.java
+++ src/main/java/com/exam/App.java
@@ -1,6 +1,7 @@
 package com.exam;
+
 public class App {
-  pubic static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("======================================================================");
     System.out.println("Jenkins Build Syccessfull-----Manikumar Kundena");
     System.out.println("=======================================================================");
